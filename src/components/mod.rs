e21ee5920c0cc8f70a9075647672d844ca5917eb
Maxim Menshikov <maxim@menshikov.org>
2024-06-09 21:06:32 +0100

    Add module view

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -2,5 +2,6 @@ pub mod baloon;
 pub mod config_view;
 pub mod device_view;
 pub mod device_group_view;
+pub mod module_view;
 pub mod pagination;
 pub mod user_view;
---
+++ src/components/module_view.rs
@@ -0,0 +1,52 @@
+use isabelle_dm::data_model::item::Item;
+use yew::prelude::*;
+
+pub struct ModuleView;
+
+impl Component for ModuleView {
+    type Message = ();
+    type Properties = Item;
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {}
+    }
+
+    fn changed(&mut self, _ctx: &Context<Self>) -> bool {
+        true
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div class="list-item">
+                <div class="list-item-image">
+                  <figure class="image is-64x64">
+                    <img class="is-rounded" src="/avatar.png"/>
+                  </figure>
+                </div>
+
+                <div class="list-item-content">
+                    <div class="list-item-title pr-5"><a href={ "/module/edit?id=".to_owned() + &ctx.props().id.to_string() }>{ ctx.props().safe_str("name", "").clone()}</a>
+                        <span class="pl-5">
+                            <span class={"tag is-danger ".to_string() + if ctx.props().safe_bool("role_is_active", true) { "is-hidden" } else { "" }}>{ "Inactive" }</span>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="list-item-controls">
+                  <div class="buttons is-right">
+                    <a href={ "/module/edit?id=".to_owned() + &ctx.props().id.to_string() }>
+                        <button class="button is-link is-outlined">
+                          <span>{"Edit"}</span>
+                        </button>
+                    </a>
+                    <a href={ "/itm/edit?next=module_list&del=true&collection=module&id=".to_owned() + &ctx.props().id.to_string() }>
+                        <button class="button is-danger is-outlined">
+                          <span>{"Remove"}</span>
+                        </button>
+                    </a>
+                  </div>
+                </div>
+            </div>
+        }
+    }
+}
